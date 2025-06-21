2bd0e3ee9fbedc5269750d84ca9a6398c7ea1c8a
Chanuka999 <chanukaranditha99@gmail.com>
2025-06-21 16:44:16 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl">WELCOME TO MY 3D-PORTFOLILO</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
