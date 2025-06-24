8a5e069681b7ca7ccbd4bad7f2045b8875109ed1
Chanuka999 <chanukaranditha99@gmail.com>
2025-06-25 01:53:21 +0530

    Refactor App component to improve structure by rearranging sections and ensuring proper imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import Navbar from "./components/Navbar";
+import LogoSection from "./sections/LogoSection";
+import FeatureCards from "./sections/FeatureCards";
+import ExperienceSection from "./sections/ExperienceSection";
+import Education from "./components/Education";
+import Cetificates from "./components/Cetificates";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Education />
       <ShowcaseSection />
+      <Cetificates />
+      <LogoSection />
+
+      <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
