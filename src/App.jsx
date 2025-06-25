274f8d5cf57aa8811bbe04d28d6e905b02615906
Chanuka999 <chanukaranditha99@gmail.com>
2025-06-26 00:39:38 +0530

    Refactor App component to include Information and Skills sections, and remove ExperienceSection

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import Education from "./components/Education";
 import Cetificates from "./components/Cetificates";
+import Skills from "./components/Skills";
+import Information from "./components/Information";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Information />
       <Education />
       <ShowcaseSection />
+      <Skills />
       <Cetificates />
       <LogoSection />
 
       <FeatureCards />
-      <ExperienceSection />
     </>
   );
 };
